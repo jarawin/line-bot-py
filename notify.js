27d3b0149dd12da32480e0e1839785f26f2564a3
jarawin <79744156+jarawin@users.noreply.github.com>
2021-07-11 18:41:09 +0700

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,16 +1,14 @@
-function sendLineNotify(text, image) {
+function sendLineNotify(text) {
   var messages = {
     message: text,
-    imageThumbnail: image,
-    imageFullsize: image,
   };
   var url = "https://notify-api.line.me/api/notify";
-  var token = "p9KEHl2DpVLfRgN0QH4q36ZfF8Ku7xgU5lpm3GbrVrD";
+  var token = ""; // INPUT TOKEN HERE
   var options = {
     method: "post",
     payload: messages,
     headers: {
-      Authorization: "Bearer " + token,
+      Authorization: `Bearer ${token}`,
     },
   };
   fetch(url, options)
@@ -23,6 +21,14 @@ function sendLineNotify(text, image) {
     });
 }
 
+// ?? how to get line notify token
+// 1. https://notify-bot.line.me/th/
+// 2. click เข้าสู่ระบบ และ login ด้วย line ตนเอง
+// 3. click ที่ชื่อไลน์ตัวเองมุมขวาบน
+// 4. click หน้าของฉัน
+// 5. เลื่อนมาข้างล่างสุด ==> กดออกโทเคน
+// 6. ใส่ข้อความอะไรก็ได้
+// 7. กด รับการแจ้งเตือนแบบตัวต่อตัวจาก LINE Notify
+// 8. กด คัดลอก token มาวางในบรรทัดที่ 8
 
-
-export { sendLineNotify };
+sendLineNotify(text)
